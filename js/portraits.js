2108f8016fd7f0c4396e58d894be48ed75244bf6
jsjang0104 <jsjang07028@gmail.com>
2026-09-20 01:09:09 +0900

    change name

--- js/portraits.js
+++ js/portraits.js
@@ -5,7 +5,7 @@ export const DEFAULT_FISH = [
     name: '채붕이',
     src: './KakaoTalk_Photo_2026-09-14-21-55-43.jpeg',
     color: PALETTE[0],
-    crop: { x: 0.45, y: 0.34, zoom: 1.8 },
+    crop: { x: 0.45, y: 0.34, zoom: 2.0 },
   },
   {
     id: 'friend-2',
@@ -16,17 +16,17 @@ export const DEFAULT_FISH = [
   },
   {
     id: 'friend-3',
-    name: '장꽉수',
+    name: '꽉수',
     src: './KakaoTalk_Photo_2026-09-14-21-57-38.jpeg',
     color: PALETTE[2],
-    crop: { x: 0.54, y: 0.49, zoom: 2.5 },
+    crop: { x: 0.54, y: 0.49, zoom: 3.0 },
   },
   {
     id: 'friend-4',
     name: '하붕이',
     src: './KakaoTalk_Photo_2026-09-14-23-38-10.jpeg',
     color: PALETTE[3],
-    crop: { x: 0.42, y: 0.645, zoom: 1.3 },
+    crop: { x: 0.42, y: 0.645, zoom: 1.5 },
   },
   {
     id: 'friend-5',
@@ -40,28 +40,28 @@ export const DEFAULT_FISH = [
     name: '아그다',
     src: './KakaoTalk_Photo_2026-09-16-22-58-49.jpeg',
     color: PALETTE[4],
-    crop: { x: 0.515, y: 0.465, zoom: 2.5 },
+    crop: { x: 0.515, y: 0.465, zoom: 2.7 },
   },
   {
     id: 'friend-7',
     name: '홍햄',
     src: './KakaoTalk_Photo_2026-09-17-17-21-11.jpeg',
     color: PALETTE[1],
-    crop: { x: 0.5, y: 0.47, zoom: 1.5 },
+    crop: { x: 0.5, y: 0.47, zoom: 1.7 },
   },
   {
     id: 'friend-8',
     name: '레전드 세일러문 하츠투하츠 쵀정우',
     src: './KakaoTalk_Photo_2026-09-20-00-24-57.jpeg',
     color: PALETTE[2],
-    crop: { x: 0.53, y: 0.505, zoom: 1.12 },
+    crop: { x: 0.53, y: 0.505, zoom: 1.5 },
   },
   {
     id: 'friend-9',
-    name: '이  강  준',
+    name: '"이   강   준"',
     src: './KakaoTalk_Photo_2026-09-20-00-29-15.jpeg',
     color: PALETTE[3],
-    crop: { x: 0.49, y: 0.415, zoom: 1.12 },
+    crop: { x: 0.49, y: 0.415, zoom: 1.5 },
   },
 ];
 const clamp = (n, min, max) => Math.max(min, Math.min(max, n));
